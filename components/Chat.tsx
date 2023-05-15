e63c8fd2eab85daf64b2b5e50a5f9d85839615cd
Marco Marchionni <marco.marchionni@gmail.com>
2023-05-15 11:37:42 +0200

    Added chat interface

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,22 +1,44 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View, Text } from "react-native";
 import { StackParamList } from "../utils/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { GiftedChat, IMessage} from 'react-native-gifted-chat';
 
 type ChatProps = NativeStackScreenProps<StackParamList, "Chat">;
 
 const Chat = ({ route, navigation }: ChatProps) => {
   const { name, backgroundColor } = route.params;
+  const [ messages, setMessages ] = useState<IMessage[]>()
 
-  // Set the name chose by the user as screen title
   useEffect(() => {
+    // Set the name chose by the user as screen title
     navigation.setOptions({ title: name });
+    // Init static message
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
   }, []);
+
+  const onSend = (newMessages: IMessage[]) => {
+    setMessages((previousMessages)=> GiftedChat.append(previousMessages, newMessages))
+  }
 
   return (
     // The background color is set according to the user's choice
     <View style={[styles.container, { backgroundColor }]}>
-      <Text style={{ fontSize: 20, fontWeight: "600" }}>Chat!</Text>
+      <GiftedChat 
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{_id: 1}}/>
     </View>
   );
 };
@@ -24,8 +46,6 @@ const Chat = ({ route, navigation }: ChatProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
 
